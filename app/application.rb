7da12fefa9b2a20b4d60ad5c2f60c3b653536c9d
github username <github email address>
2018-10-06 23:52:32 -0400

    Built a greeter

--- app/application.rb
+++ app/application.rb
@@ -2,25 +2,19 @@ class Application
 
   def call(env)
 
-  res = Rack::Response.new()
-  # 1. If it's before noon, greet the user with "Good Morning!"
-  # 2. If it's after noon, greet the user with "Good Afternoon!".
-  #Remember that getting the time may return results in 24 hour time! Anytime after 12 is the afternoon.
-    day_times = Time.now
-    day_times.inspect
+  res = Rack::Response.new
+  # # 1. If it's before noon, greet the user with "Good Morning!"
+  # # 2. If it's after noon, greet the user with "Good Afternoon!".
+  # #Remember that getting the time may return results in 24 hour time! Anytime after 12 is the afternoon.
+    day_hours = Time.now.utc.to_s.split(" ")[1].split(":")[0].to_i
+    #day_times.inspect
 
-    morning = Kernel.rand(0..12)
-    afternoon = Kernel.rand(12..24)
+       if day_hours.to_i <= 12
+          res.write "Good Morning!"
+        else
+         res.write "Good Afternoon!"
+        end
 
-    if day_time <= 12
-      res.write "Good Morning!"
-     #res.write "Good Morning! It is #{morning} am "
-  else
-      res.write "Good Afternoon!"
-     #res.write "Good Afternoon! It is #{afternoon} pm"
-    end
-
-   res.finish
-  end
-
+       res.finish
+      end
 end
